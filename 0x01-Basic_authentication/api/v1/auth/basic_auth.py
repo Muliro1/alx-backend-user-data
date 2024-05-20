b6139ac3e164b8af1f9b1f7d68a18aa221eb110a
Muliro1 <mulirokhaemba@gmail.com>
2024-05-20 18:11:10 +0300

    style

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,5 +5,6 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """BasicAuth class, which is a subclass of Auth. It implements a basic auth system for the API."""
+    """BasicAuth class, which is a subclass of Auth.
+    It implements a basic auth system for the API."""
     pass
