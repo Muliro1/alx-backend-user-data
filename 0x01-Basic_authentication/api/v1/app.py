97ca0c329e296175b7417257cc00260727d1953e
Muliro1 <mulirokhaemba@gmail.com>
2024-05-20 19:19:38 +0300

    typing

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,6 +3,7 @@
 Route module for the API
 """
 from os import getenv
+from typing import Dict, Tuple
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request, abort
 from flask_cors import CORS, cross_origin
