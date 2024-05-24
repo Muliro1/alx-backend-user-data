23cc7fc46ff3c7e2c6693ac6b20d6571d9f21fcc
Muliro1 <mulirokhaemba@gmail.com>
2024-05-24 22:36:11 +0300

    j

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,9 +20,15 @@ class SessionAuth(Auth):
     """
     user_id_by_session_id = {}
 
-    def create_session(self, user_id: str = None): -> str:
+    def create_session(self, user_id: str = None) -> str: 
         if not user_id or not isinstance(user_id, str):
             return
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+    
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        if not session_id or not isinstance(session_id, str):
+            return
+        user_id = self.user_id_by_session_id.get(session_id)
+        return user_id
