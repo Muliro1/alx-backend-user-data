5ad4e86805d62e960d2a0fa0ab1b8974040de0e7
Muliro1 <mulirokhaemba@gmail.com>
2024-05-24 23:34:23 +0300

    l

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,20 +1,46 @@
 #!/usr/bin/env python3
+""" Module of Users views
 """
-Module for Basic Authentication
-"""
-from base64 import b64decode
-import uuid
-from typing import Optional, TypeVar
-from api.v1.auth.auth import Auth
+import os
+from flask import abort, jsonify, request
+from api.v1.views import app_views
 from models.user import User
 
 
-class SessionAuth(Auth):
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def auth_session():
     """
-    Class that handles basic authentication
+    Handle user login
+    Return:
+        dictionary representation of user if found else error message
     """
-    pass
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or email == '':
+        return jsonify({"error": "email missing"}), 400
+    if password is None or password == '':
+        return jsonify({"error": "password missing"}), 400
+    users = User.search({"email": email})
+    if not users or users == []:
+        return jsonify({"error": "no user found for this email"}), 404
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            resp = jsonify(user.to_json())
+            session_name = os.getenv('SESSION_NAME')
+            resp.set_cookie(session_name, session_id)
+            return resp
+    return jsonify({"error": "wrong password"}), 401
 
 
-if __name__ == '__main__':
-    pass
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def handle_logout():
+    """
+    Handle user logout
+    """
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)
