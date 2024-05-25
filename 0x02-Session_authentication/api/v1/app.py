83f16291776df0ab80907aab479728bc7974635a
Muliro1 <mulirokhaemba@gmail.com>
2024-05-25 06:07:46 +0300

    k

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -2,20 +2,18 @@
 """
 Route module for the API
 """
+from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from os import getenv
 
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-AUTH_TYPE = getenv("AUTH_TYPE")
-
+AUTH_TYPE = os.getenv("AUTH_TYPE")
 if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
@@ -33,98 +31,48 @@ elif AUTH_TYPE == "session_db_auth":
     auth = SessionDBAuth()
 
 
-@ app.errorhandler(404)
-def not_found(error) -> tuple:
-    """ Handle 404 errors by returning a JSON response
+@app.before_request
+def bef_req():
+    """
+    Filter each request before it's handled by the proper route
+    """
+    if auth is None:
+        pass
+    else:
+        setattr(request, "current_user", auth.current_user(request))
+        excluded = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
+        ]
+        if auth.require_auth(request.path, excluded):
+            cookie = auth.session_cookie(request)
+            if auth.authorization_header(request) is None and cookie is None:
+                abort(401, description="Unauthorized")
+            if auth.current_user(request) is None:
+                abort(403, description="Forbidden")
 
-    Args:
-        error (flask.utils.RequestException): Exception instance
 
-    Returns:
-        tuple: A tuple containing a JSON response
-        and a 404 status code
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
-@ app.errorhandler(401)
-def unauthorized_error(error) -> tuple:
-    """
-    Unauthorized handler
-
-    Args:
-        error (flask.utils.RequestException): Exception instance
-
-    Returns:
-        tuple: A tuple containing a JSON response and a 401 status code
-
-    Description:
-        This function is used to handle unauthorized errors.
-        It returns a JSON response with an "error"
-        key set to "Unauthorized"
-        and a 401 status code.
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Request unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
-@ app.errorhandler(403)
-def forbidden_error(error) -> tuple:
-    """Forbidden handler
-
-    This function is used to handle 403 errors.
-    It returns a JSON response with an "error"
-    key set to "Forbidden"
-    and a 403 status code.
-
-    Args:
-        error (flask.utils.RequestException): Exception instance
-
-    Returns:
-        tuple: A tuple containing a JSON response and a 403 status code
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Request unauthorized handler
     """
     return jsonify({"error": "Forbidden"}), 403
-
-
-@ app.before_request
-def before_request() -> str:
-    """ Before Request Handler
-
-    This function is called before each request.
-    It checks if the request is authorized (i.e., the
-    request has a valid authentication token or a valid
-    session cookie).
-
-    If the request is not authorized, it raises a 401
-    error. If the request is authorized, it sets the
-    `current_user` attribute of the request object to
-    the current user.
-
-    Args:
-        request (Request): The Flask request object
-
-    Returns:
-        str: None
-    """
-    if auth is None:
-        return
-
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/',
-                      '/api/v1/auth_session/login/']
-
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-
-    if auth.authorization_header(request) is None \
-            and auth.session_cookie(request) is None:
-        abort(401)
-
-    current_user = auth.current_user(request)
-    if current_user is None:
-        abort(403)
-
-    request.current_user = current_user
 
 
 if __name__ == "__main__":
--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -11,8 +11,15 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Creates a Session ID for a user_id"""
+        """
+        Creates a Session ID for a user_id
 
+        Args:
+            user_id (str): user id
+
+        Returns:
+            Session ID in string format or None if user_id is None or not a string
+        """
         if user_id is None or not isinstance(user_id, str):
             return None
 
@@ -23,16 +30,30 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Returns a User ID based on a Session ID"""
+        """
+        Returns a User ID based on a Session ID
 
+        Args:
+            session_id (str): session ID
+
+        Returns:
+            user id or None if session_id is None or not a string
+        """
         if session_id is None or not isinstance(session_id, str):
             return None
 
         return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None):
-        """Returns a User instance based on a cookie value"""
+        """
+        Returns a User instance based on a cookie value
 
+        Args:
+            request : request object containing cookie
+
+        Returns:
+            User instance or None if session_id is None
+        """
         session_id = self.session_cookie(request)
 
         if session_id is None:
@@ -43,8 +64,15 @@ class SessionAuth(Auth):
         return User.get(user_id)
 
     def destroy_session(self, request=None):
-        """Deletes de user session / logout"""
+        """
+        Deletes de user session / logout
 
+        Args:
+            request : request object containing cookie
+
+        Returns:
+            True if session is deleted, False otherwise
+        """
         if request is None:
             return False
 
